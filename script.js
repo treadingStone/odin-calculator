52557805a540a6f0f2f0b85a1f85b6634bf13db7
treadingStone <204741967+treadingStone@users.noreply.github.com>
2025-04-08 16:14:56 -0400

    add fleshed out calculator logic

--- script.js
+++ script.js
@@ -7,51 +7,92 @@ let operator = '';
 let result = '';
 
 function operate() {
-    const x = parseInt(firstNumber);
-    const y = parseInt(secondNumber);
+    let x = 0;
+    let y = 0;
+    if (firstNumber.includes('.')) {
+        x = parseFloat(firstNumber);
+    } else {
+        x = parseInt(firstNumber);
+    };
+    if (secondNumber.includes('.')) {
+        y = parseFloat(secondNumber);
+    } else {
+        y = parseInt(secondNumber);
+    };
     if (operator === '+') {
-        return x + y;
+        return (x + y);
     } else if (operator === '-') {
-        return x - y;
+        return (x - y);
     } else if (operator === '*') {
-        return x * y;
+        return (x * y);
     } else if (operator === '/') {
-        return x / y;
+        if (y === 0) {
+            alert('Are you trying to destroy the universe!?');
+            return 'ERROR';
+        } else {
+            return (x / y);
+        };
     };
 };
 
+function reset() {
+    firstNumber = '';
+    secondNumber = '';
+    operator = '';
+    result = '';
+    updateUI();
+};
+
 function updateUI() {
-    textDisplay.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+    textDisplay.textContent = `${firstNumber !== '' ? firstNumber : '0'} ${operator} ${secondNumber}`;
 };
 
 buttons.addEventListener('click', (e) => {
     const buttonText = e.target.textContent;
     const buttonClasses = e.target.classList;
-    if (buttonClasses.contains('number')) {
-        if (operator === '' && firstNumber === '') {
-            firstNumber += buttonText;
-        } else {
-            secondNumber += buttonText;
+    if (buttonText === 'C') {
+        reset();
+    } else {
+        if (buttonClasses.contains('number')) {
+            if (operator === '') {
+                firstNumber += buttonText;
+            } else {
+                secondNumber += buttonText;
+            };
+        } else if (buttonText === '.') {
+            if (operator === '') {
+                if (!firstNumber.includes(buttonText)) {
+                    firstNumber += buttonText;
+                };
+            } else if (!secondNumber.includes(buttonText)) {
+                secondNumber += buttonText;
+            } else {
+                alert('There is already a decimal!');
+            }
+        } else if (buttonText === '-' || buttonText ===  '+' || buttonText ===  '*' || buttonText ===  '/') {
+            if (operator !== '') {
+                alert('There is already an operator!');
+                return
+            } else if (!firstNumber) {
+                alert('You need a number to start with!');
+                return
+            } else {
+                operator += buttonText;
+            };
+        } else if (buttonText === '=') {
+            if (firstNumber !== '' && operator !== '' && secondNumber !== '') {
+                if (operate() !== 'ERROR') {
+                    result = operate().toFixed(2);
+                    firstNumber = result;
+                    secondNumber = '';
+                    operator = '';
+                } else {
+                    reset();
+                }
+            } else {
+                alert('I think you\'re missing something!');
+            };
         };
-    } else if (buttonText === '-' || buttonText ===  '+' || buttonText ===  '*' || buttonText ===  '/') {
-        if (operator !== '') {
-            alert('There is already an operator!');
-            return
-        } else if (!firstNumber) {
-            alert('You need a number to start with!');
-            return
-        } else {
-            operator += buttonText;
-        };
-    } else if (buttonText === '=') {
-        if (firstNumber !== '' && operator !== '' && secondNumber !== '') {
-            result = operate();
-            firstNumber = result;
-            secondNumber = '';
-            operator = '';
-        } else {
-            alert('I think you\'re missing something!');
-        }
+        updateUI();
     };
-    updateUI();
 });
